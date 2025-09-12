871a1fc3cbc974590fa2bcde8898d9c6749eca5b
NiriLorentzen <niri.lorentzen@gmail.com>
2025-09-12 14:57:57 +0200

    la til et stylesheet

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
 <body>
-    <h1>Prosjekt</h1>
+    <h1>BookFinder</h1>
+    <p>inndata stuff</p>
 </body>
 </html>
