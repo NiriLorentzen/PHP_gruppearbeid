2006dc54377b9f5511f5ee1f8a2240772664ec6d
Eivind Hovden Breivik <eivindhb@uia.no>
2025-10-08 12:32:47 +0200

    La til kommentarer i koden og pagecount i recommendations

--- booksAPI.php
+++ booksAPI.php
@@ -1,16 +1,19 @@
 <?php 
 
+//Api call to google books too fetch book information based on recommendation query
     function getBookRecommendation($bookQuery) {
         $apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" . urlencode($bookQuery);
 
-
+//Gets json info from the api. Checks if failed and returns error if false
         $response = file_get_contents($apiUrl);
         if($response === FALSE) {
             return ["error" => "Kan ikke hente data fra Google Books"];
         }
 
+//Decodes json        
         $bookData = json_decode($response, true); 
 
+//Sets data for each recommended book
         $recommendations = []; 
 
         if (isset($bookData['items'])) {
@@ -21,13 +24,13 @@
                 "title" => $volumeInfo['title'] ?? 'Ukjent tittel',
                 "authors" => $volumeInfo['authors'][0] ?? 'Ukjent forfatter',
                 "description" => $volumeInfo['description'] ?? 'Ingen beskrivelse',
+                "pageCount" => $volumeInfo['pageCount'] ?? 'Ukjent mengde sider',                
                 "thumbnail" => $volumeInfo['imageLinks']['thumbnail'] ?? null
             ];
         }
     }
 
         return $recommendations;
-
     }
     
     header('Content-Type: application/json');
