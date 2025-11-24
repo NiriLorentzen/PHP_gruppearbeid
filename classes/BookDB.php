6bfa7f820206cbf24e6887cf4d2e36dbc51ef0b0
EivindHB <eivind.h.breivik@gmail.com>
2025-11-24 20:15:30 +0100

    fikset feil i value oppføring i insertBook og la til userFetchAllBooks funksjonen

--- classes/BookDB.php
+++ classes/BookDB.php
@@ -10,10 +10,10 @@ class BookDB {
     }
 
     public function insertBook($data) {
-
+        //KAN VÆRE LURT Å ENDRE TIL ON DUPLICATE KEY UPDATE?
         $q = $this->pdo->prepare( //insert ignore sørger for at flere brukere kan sette inn samme bok, og at bøkene oppdaterer seg i database om mulig
             "INSERT IGNORE INTO books(bookID, book_name, author, book_description, page_count) 
-            VALUES(:bookID, :book_name, :author, book_description, page_count)"
+            VALUES(:bookID, :book_name, :author, :book_description, :page_count)"
         );
         $q->bindParam(':bookID', $data['bookID']); 
         $q->bindParam(':book_name', $data['book_name']);
@@ -36,11 +36,24 @@ class BookDB {
     public function userRemoveBook($userID, $bookID) {
         $q = $this->pdo->prepare(
             "DELETE FROM user_books 
-            WHERE userID AND bookID(:userID, :bookID)"
+            WHERE userID = :userID AND bookID = bookID"
         );    
         $q->bindparam(':userID', $userID);
         $q->bindparam(':bookID', $bookID);
         $q->execute();
     }
+
+    public function userFetchAllBooks($userID) {
+        $q = $this->pdo->prepare(
+            "SELECT * FROM books b
+            INNER JOIN user_books ub ON  b.bookID = ub.bookID
+            WHERE ub.userID = :userID"
+        );
+        $q->bindparam(':userID', $userID);   
+        $q->execute();
+        return $q->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+    
 }
 ?>
