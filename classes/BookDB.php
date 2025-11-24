1e0befd3b2dfb848d812feaf88902916845b22b6
EivindHB <eivind.h.breivik@gmail.com>
2025-11-24 19:59:07 +0100

    fjernet overfladisk logikk

--- classes/BookDB.php
+++ classes/BookDB.php
@@ -23,49 +23,24 @@ class BookDB {
         $q->execute();
     }
 
+    public function userAddBook($userID, $bookID) {
+        $q = $this->pdo->prepare(
+            "INSERT IGNORE INTO user_books(bookID, userID)
+            VALUES(:bookID, :userID)"
+        );    
+        $q->bindparam(':userID', $userID);
+        $q->bindparam(':bookID', $bookID);
+        $q->execute();
+    }
+
+    public function userRemoveBook($userID, $bookID) {
+        $q = $this->pdo->prepare(
+            "DELETE FROM user_books 
+            WHERE userID AND bookID(:userID, :bookID)"
+        );    
+        $q->bindparam(':userID', $userID);
+        $q->bindparam(':bookID', $bookID);
+        $q->execute();
+    }
 }
-
-if(!$input) {
-    //GJØR EN ERROR KODE ELLERNO HER
-    exit;
-}
-
-try {
-    $pdo->beginTransaction();
-
-    $qIns = $pdo->prepare( //insert ignore sørger for at flere brukere kan sette inn samme bok, og at bøkene oppdaterer seg i database om mulig
-        "INSERT IGNORE INTO books(bookID, book_name, author, book_description, page_count) 
-        VALUES(:bookID, :book_name, :author, book_description, page_count)"
-    );
-    $qIns->bindParam(':bookID'); //HVOR KOMMER DATAEN FRA
-    $qIns->bindParam(':book_name');
-    $qIns->bindParam(':author');
-    $qIns->bindParam(':book_description');
-    $qIns->bindParam(':page_count');
-    $qIns->execute();
-
-    $bookID = $pdo->lastInsertId();
-
-    $qInsUserBook = $pdo->prepare(
-        "INSERT INTO user_books(bookID, userID)
-        VALUES(:bookID, :userID)"
-    );
-    $qInsUserBook->bindparam(':bookID', $bookID);
-    $qInsUserBook->bindparam(':userID', ); //PUTT INN USER ID
-
-
-    $pdo->commit();
-
-} catch(PDOException $e) {
-    $pdo->rollBack();
-
-    error_log('Tid: ' . date("Y-m-d H:i:s") . ' Database error: ' . $e->getMessage());           
-    die("Beklager, det oppstod en feil ved lagring av data."); //Gis til bruker uten info om logikk.
-
-}
-
-
-
-
-
 ?>
