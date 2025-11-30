530f6761c02146721b8dc90c8cf5a1d168c94c99
nirilorentzen <niri.lorentzen@gmail.com>
2025-11-30 16:58:27 +0100

    flere kommentarer

--- Scripts/configExample.php
+++ Scripts/configExample.php
@@ -1,5 +1,5 @@
 <?php
-    //eksempelfil av hvordan config ser ut for å vise og dele på github, siden den er gjemt av .gitignore for å ikke dele den faktiske informasjonen
+    //ikke innfyllt eksempelfil av hvordan config ser ut for å vise og dele på github, siden den er gjemt av .gitignore for å ikke dele den faktiske informasjonen
 
     $Gemini_API_key = "api-key";
     
