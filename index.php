0aaa20486530ae12179d9fbd79db2e5cde4b3250
Eivind-HB <eivind.h.breivik@gmail.com>
2025-12-01 14:48:57 +0100

    Forbedret smått på tekst og sentrerte teksten

--- index.php
+++ index.php
@@ -11,16 +11,18 @@
     <link rel="stylesheet" href="css/stylesheet.css">
 </head>
 <body>
+<div class="welcomePage">
     <h1>Velkommen til BookFinder!</h1>
 
-    <p>Er du utkikk etter nye bøker? 
-        Eller trenger du et sted å lagre bøker du planlegger å lese? <br><br>
-        Da har du kommet til rett sted!
+    <p>Er du utkikk etter nye bøker? Sliter du med å finne den som er perfekt for deg?
+        Eller kanskje trenger du et sted å lagre bøkene du planlegger å lese? <br><br>
+        <b>Da har du kommet til rett sted!</b><hr><br>
         
-        Snakk med vår AI bibliotekar for bokanbefalinger basert på dine preferanser,
+        Snakk med vår AI bibliotekar for bokanbefalinger basert på dine preferanser, 
         eller søk i vår omfattende bokdatabase. <br><br>
         
         Slå deg løs, finn deg en bok, og opprett en konto for å lagre dine favorittbøker i din personlige bokhylle.
     </p>
+</div>
 </body>
 </html>
